e7290c4999f4e8852c664873164078576f7f984d
phil <phil@branch14.org>
2019-11-27 14:45:19 +0100

    route events way more sophisticated

--- lib/gh/events/slack.rb
+++ lib/gh/events/slack.rb
@@ -7,6 +7,10 @@ module GH::Events::Slack
 
   extend self
 
+  # this is a carefully curated list of fields in gh events that might
+  # helpful when filtering events
+  ASPECTS = %i[type action state context]
+
   def translate(payload)
     event = JSON.parse(payload, object_class: OpenStruct)
     type = GH::Events.typeof(event).to_s
@@ -16,8 +20,29 @@ module GH::Events::Slack
     event.ref_type = 'tag' if event.ref&.match(/^refs\/tags\//)
     event.ref = event.ref&.split('/')&.last
 
-    template = templates[[type, event.action]*'.']
-    template ||= (templates[type] || "No template for type: #{type}.")
+    # add type to the event
+    event.type = type
+
+    # collect the aspects
+    aspects = ASPECTS.map { |a| event.send(a) }.compact
+
+    # add the specific type (stype) to the event
+    event.stype = aspects * '.'
+
+    # lookup the template by type and all the specific types
+    _result = aspects.reduce({as: [], ts: []}) do |r, aspect|
+      r[:as] << aspect
+      r[:ts] << templates[r[:as] * '.']
+      r
+    end
+    template = _result[:ts].compact.last
+
+    # if the event was set to `false` abort
+    exit(1) if template === false
+
+    # if there is no template use a default
+    template ||= templates['no_template']
+
     render(template, event)
   end
 
--- lib/gh/events/version.rb
+++ lib/gh/events/version.rb
@@ -1,5 +1,5 @@
 module GH
   module Events
-    VERSION = "0.5.0"
+    VERSION = "0.6.0"
   end
 end
